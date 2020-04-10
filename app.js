b59d0dd77df81419996a0907f44e866f54a03240
yatabis <39yatabis.rim8820xxx@gmail.com>
2020-04-10 18:40:38 +0900

    add: ホームにバッジを追加

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = new Vue({
   data: {
     tab: "home",
     email: "39yatabis.work@gmail.com",
+    updated_at: ""
+  },
+  created: function () {
+    fetch("https://api.github.com/repos/yatabis/portfolio")
+      .then(response => response.json())
+      .then(json => {
+        this.updated_at = "https://img.shields.io/badge/last_update-"
+                        + json.updated_at.split("T")[0].replace(/-/g,".")
+                        + "-blue"
+      })
   }
 })
 
